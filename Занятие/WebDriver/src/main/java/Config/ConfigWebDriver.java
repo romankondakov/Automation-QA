9b51d8068f1d1a2deb4b20c0180be136317b7e3c
Roman Kondakov <kondakovroman487@gmail.com>
2023-05-30 13:41:08 +0300

    new driver

--- Занятие/WebDriver/src/main/java/Config/ConfigWebDriver.java
+++ Занятие/WebDriver/src/main/java/Config/ConfigWebDriver.java
@@ -4,9 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ConfigWebDriver {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        URL mainUrl=null;
+        try {
+            mainUrl=new URL("https://dnipro.ithillel.ua/");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
@@ -14,5 +24,10 @@ public class ConfigWebDriver {
         options.addArguments("--incognito");
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://dnipro.ithillel.ua/");
+
+        driver.navigate().to(mainUrl);
+        System.out.println(driver.getTitle());
+        Thread.sleep(3000);
+        driver.quit();
     }
 }
