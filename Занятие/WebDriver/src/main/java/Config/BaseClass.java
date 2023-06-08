ad77bd94705b20812f9b8ce518a3bfb27efaee03
Roman Kondakov <kondakovroman487@gmail.com>
2023-06-08 11:23:10 +0300

    new HW

--- Занятие/WebDriver/src/main/java/Config/BaseClass.java
+++ Занятие/WebDriver/src/main/java/Config/BaseClass.java
@@ -1,8 +1,22 @@
 package Config;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 
 public class BaseClass {
 
-   static public WebDriver driver = ConfigWebDriver.createDriver(WebDrivers.CHROME_INCOGNITO);
+   public static  WebDriver driver;
+   @BeforeClass
+   public static void beforeClass(){
+
+      driver = ConfigWebDriver.createDriver(WebDrivers.CHROME_INCOGNITO);
+   }
+
+   @AfterClass
+   public static void afterClass() throws InterruptedException {
+
+      Thread.sleep(10000);
+      driver.quit();
+   }
 }
---
+++ Занятие/WebDriver/src/test/java/HW_Find_Teachers.java
@@ -0,0 +1,47 @@
+import Config.BaseClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class HW_Find_Teachers extends BaseClass {
+
+    static String mainURL = "https://dnipro.ithillel.ua/";
+    By coursePrograming = By.xpath("//div[@class='course-cat-bar_body']//img[@alt='Програмування']");
+    By courseFront = By.xpath("//*[@id='categories']/div[3]/div/ul/li[1]/div/div[1]/ul/li[1]/a/div[2]");
+
+
+    ;
+
+    @BeforeClass
+    public static void first() {
+        driver.get(mainURL);
+    }
+
+    @Before
+    public void mainP() {
+        if (!driver.getCurrentUrl().equals(mainURL)) {
+            driver.navigate().to(mainURL);
+        }
+    }
+
+    @Test
+    public void mainPage() {
+        driver.findElement(coursePrograming).click();
+        driver.findElement(courseFront).click();
+        List<WebElement> teachersContainer = (List<WebElement>) driver.findElement(By.xpath("//li[@class='coach-list_item]"));
+        System.out.println("Teachers List:");
+        for (WebElement teacher : teachersContainer) {
+            System.out.println(teacher.findElement(By.className("coach-card_name")));
+    }
+
+//    @Test
+//    public void teachersList() {
+//
+//        }
+    }
+}
